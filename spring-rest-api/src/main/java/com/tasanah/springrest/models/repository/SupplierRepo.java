d5929e6ab83184d3e17eaf58801e45a7dc467284
Yoga Hilmi Tasanah <yogahilmi@gmail.com>
2022-03-30 16:52:42 +0700

    implement derived query method

--- spring-rest-api/src/main/java/com/tasanah/springrest/models/repository/SupplierRepo.java
+++ spring-rest-api/src/main/java/com/tasanah/springrest/models/repository/SupplierRepo.java
@@ -1,9 +1,19 @@
 package com.tasanah.springrest.models.repository;
 
+import java.util.List;
+
 import com.tasanah.springrest.models.entities.Supplier;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface SupplierRepo extends CrudRepository<Supplier, Long>{
+    
+    Supplier findByEmail(String email);
+    
+    List<Supplier> findByNameContainsOrderByIdDesc(String name);
+
+    List<Supplier> findByNameStartingWith(String prefix);
+
+    List<Supplier> findByNameContainsOrEmailContains(String name, String email);
     
 }
